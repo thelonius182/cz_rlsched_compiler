20008e7d75530d00a09a48e1fff77b8735c729ff
thelonius182 <lcavdakker@gmail.com>
2019-06-04 01:39:25 +0200

    more work on rl-schedule compiler

--- src/compile_rlsched_script.R
+++ src/compile_rlsched_script.R
@@ -1,6 +1,16 @@
-cur_cz_week_uzm <- cz_week %>% 
+cur_cz_week_lgm <- cz_week %>% 
   filter(weekschema == ymd_hm("2019-06-20 13:00")) %>% 
   inner_join(itunes_cupboard) %>% 
   filter(uitzend_mac_jn == "n") %>% 
-  select(-starts_with("week")) %>% 
+  filter(titel != "Thema" | hour(cz_tijdstip) != 21) %>% 
+  select(-starts_with("week"), -uitzend_mac_jn) %>% 
+  mutate(sel_pl = if_else(is.na(hh_van) | is.na(playlist_hh), titel, playlist_hh)) %>% 
+  arrange(cz_tijdstip)
+
+cur_cz_week_uzm <- cz_week %>% 
+  filter(weekschema == ymd_hm("2019-06-20 13:00")) %>% 
+  inner_join(itunes_cupboard) %>% 
+  filter(uitzend_mac_jn == "j") %>% 
+  select(-starts_with("week"), -uitzend_mac_jn) %>% 
+  mutate(sel_pl = if_else(is.na(hh_van) | is.na(playlist_hh), titel, playlist_hh)) %>% 
   arrange(cz_tijdstip)
