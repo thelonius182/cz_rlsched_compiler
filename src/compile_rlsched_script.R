19e2b5357b3ed93b7e48ca2ed9cf834ea641ee6f
thelonius182 <lcavdakker@gmail.com>
2019-07-03 23:16:06 +0200

    Nieuwe week, 4 juli 2019

--- src/compile_rlsched_script.R
+++ src/compile_rlsched_script.R
@@ -16,7 +16,7 @@ build_rl_script <- function(playlist, kleurschema) {
   sch01_lengte <- rls_lengte(playlist) %>% as_tibble
   
   # dj_voorkeur
-  sch01_dj_voorkeur <- "Default" %>% as_tibble
+  sch01_dj_voorkeur <- "standaard" %>% as_tibble
   
   # stuur_naar_dj
   sch01_stuur_naar_dj <- "ProgramTo=0" %>% as_tibble
